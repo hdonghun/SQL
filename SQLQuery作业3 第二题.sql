1e782c79d6faecc471b1f0d463a502b729051ffc
Donghun HAN/韩东勳 <67058000+hdonghun@users.noreply.github.com>
2021-10-14 17:23:18 +0900

    学校SQL课程的作业3

---
+++ SQLQuery作业3 第二题.sql
@@ -0,0 +1,6 @@
+	SELECT [building] FROM [guest].[student] s LEFT JOIN [guest].[department] d ON s.[dept_name] = d.[dept_name] 	;
+	SELECT [building] FROM [guest].[student] s LEFT JOIN [guest].[department] d ON s.[dept_name] = d.[dept_name]  GROUP BY [building];
+	SELECT dept_name FROM [guest].[department] WHERE [department].budget >= 90000;
+	SELECT dept_name,building FROM [guest].[department] WHERE building LIKE '%r'
+	SELECT * FROM [guest].[student] WHERE [dept_name]='Comp.Sci.' AND [tot_cred] =102;
+	SELECT * FROM [guest].[student] WHERE [dept_name]='Comp.Sci.';
