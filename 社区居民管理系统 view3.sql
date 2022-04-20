a019d0c78461d61dffaf18641ab482f8098afc87
Donghun HAN/韩东勳 <67058000+hdonghun@users.noreply.github.com>
2022-04-20 20:22:20 +0900

    Delete 社区居民管理系统 view3.sql

--- 社区居民管理系统 view3.sql
+++
@@ -1,5 +0,0 @@
-﻿--（3）缴纳项目查询
-Create View P_R_C(O_name,O_rid,C_id,C_num,C_tg,O_phone,P_time)
-As Select O_name,O_rid,C_id,C_num,C_tg,O_phone,P_time
-From  Payment P,Charge C,Owner O
-Where P_cid=C_id and P_oid=O.O_id
