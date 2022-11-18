db6ad49044cbdf67791d562975c8835214d0ab1a
Donghun HAN/韩东勳 <67058000+hdonghun@users.noreply.github.com>
2022-11-18 19:11:07 +0800

    Delete SQLQuery3-1.sql

--- SQLQuery3-1.sql
+++
@@ -1,6 +0,0 @@
-Use [Mysefl_Practices]
-SELECT * FROM [HumanResources].[Department]
-
-INSERT INTO [HumanResources].[Department](Name,GroupName,ModifiedDate) VALUES
-('DATA SCIENCE', 'Research and Development', '2020-09-30')
---받는 필드가 3개이면, 입력 값도 3개를 충족 시켜줘야 한다.
