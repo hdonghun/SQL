4256c4fd18fa83249910fc8c433d03206b6d19a9
Donghun HAN/韩东勳 <67058000+hdonghun@users.noreply.github.com>
2022-11-18 19:09:37 +0800

    Delete LEFT, RIGHT, SUBSTRING 함수.sql

--- LEFT, RIGHT, SUBSTRING 함수.sql
+++
@@ -1,5 +0,0 @@
---LEFT(X1,X2)
---RIGHT(X1,X2)
---SUBSTRING(X1,X2,X3)
-SELECT Comments, LEFT(Comments,10) AS L_TEST, RIGHT(Comments,10) AS R_TEST, 
-				SUBSTRING(Comments,5,15) AS S_TEST FROM [Production].[ProductReview]
