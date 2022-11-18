e72e258b14f7b6b89f936ef2e5b042b9981f77c4
Donghun HAN/韩东勳 <67058000+hdonghun@users.noreply.github.com>
2022-11-18 19:13:08 +0800

    Delete 문자함수 CHARINDEX, LEN, REPLACE.sql

--- 문자함수 CHARINDEX, LEN, REPLACE.sql
+++
@@ -1,7 +0,0 @@
-SELECT [EmailAddress], 
-REPLACE ([EmailAddress],'@','/') AS RE_TEST,
-LEN([EmailAddress]) AS LEN_TEST, CHARINDEX('@',[EmailAddress]) AS CHAR_TEST,
-LEFT([EmailAddress],LEN([EmailAddress])-CHARINDEX('@',[EmailAddress])-1) AS L_TEST,
-RIGHT([EmailAddress],LEN([EmailAddress])-CHARINDEX('@',[EmailAddress])) AS R_TEST
-FROM[Production].[ProductReview]
-
