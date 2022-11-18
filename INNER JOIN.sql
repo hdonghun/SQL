b10af49094832177495d6febe041b806892a6a65
Donghun HAN/韩东勳 <67058000+hdonghun@users.noreply.github.com>
2022-11-18 19:09:14 +0800

    Delete INNER JOIN.sql

--- INNER JOIN.sql
+++
@@ -1,6 +0,0 @@
---INNER JOIN을 명시해서, 작성
-SELECT * FROM [Production].[Product] AS PP INNER JOIN [Production].[ProductDescription] AS PD
-ON PP.PRODUCTID=PD.ProductDescriptionID
---특별히 JOIN을 안해도, 기본적으로 INNER JOIN이 된다. > INNER JOIN을 명시하지 않고 ,(쉼표)로 나타내고, 그에 따라 ON이 아니라 WHERE 조건을 사용한다.
-SELECT * FROM [Production].[Product] AS PP , [Production].[ProductDescription] AS PD
-WHERE PP.PRODUCTID=PD.ProductDescriptionID
