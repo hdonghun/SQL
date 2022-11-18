8abd02b0add03edd3f858777d4dc10bfcddf9f80
Donghun HAN/韩东勳 <67058000+hdonghun@users.noreply.github.com>
2022-11-18 19:10:31 +0800

    Delete OUTER JOIN.sql

--- OUTER JOIN.sql
+++
@@ -1,5 +0,0 @@
-SELECT * FROM [HumanResources].[EmployeeDepartmentHistory] 
-SELECT * FROM  [HumanResources].[Department]
-
-SELECT * FROM [HumanResources].[EmployeeDepartmentHistory] AS A LEFT OUTER JOIN [HumanResources].[Department] AS B
-ON A.[DepartmentID] = B.[DepartmentID]
