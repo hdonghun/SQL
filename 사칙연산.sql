d29ad69fba5e3f8bb83745c632b11236ae31ac81
Donghun HAN/韩东勳 <67058000+hdonghun@users.noreply.github.com>
2022-11-18 19:13:29 +0800

    Delete 사칙연산.sql

--- 사칙연산.sql
+++
@@ -1,3 +0,0 @@
---SELECT[StockedQty]*[OrderQty],[OrderQty]-[ReceivedQty]FROM [Purchasing].[PurchaseOrderDetail]
---SELECT*FROM [Purchasing].[PurchaseOrderDetail] WHERE [StockedQty]*[OrderQty]>30003
-SELECT[CountryRegionCode],[Name],[CountryRegionCode]+' AND '+[Name] FROM[Person].[CountryRegion]
