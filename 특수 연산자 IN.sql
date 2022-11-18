abb927aa4e1a21c51197b872ecbea690ffc1ac27
Donghun HAN/韩东勳 <67058000+hdonghun@users.noreply.github.com>
2022-11-18 19:13:37 +0800

    Delete 특수 연산자 IN.sql

--- 특수 연산자 IN.sql
+++
@@ -1,4 +0,0 @@
---in은 복수의 조건검색이 가능하다, OR의 확장 연산자이다.
---SELECT*FROM [Sales].[Store] WHERE [SalesPersonID]=277 OR [SalesPersonID]=283 OR [SalesPersonID]=290 
---SELECT*FROM [Sales].[Store] WHERE [SalesPersonID] IN (277,281,283,290) 
-SELECT*FROM [Sales].[Store] WHERE [SalesPersonID] NOT IN (277,281,283,290)
