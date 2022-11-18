ef9482e0337999253c62227ea9e55be8ef0a7166
Donghun HAN/韩东勳 <67058000+hdonghun@users.noreply.github.com>
2022-11-18 19:10:38 +0800

    Delete SQL 机房作业7 我的答案 第一题.sql

--- SQL 机房作业7 我的答案 第一题.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO TOY
-VALUES
-(001,'Tiro',8, 'Ultramn'),
-(002,'Tiga',7.5,'Ultramn'),
-(003,'Eis',7,'Ultramn'),
-(011,'cat',5,'Robot'),
-(012,'dog',6,'Robot'),
-(013,'rabbit',5.5,'Robot');
