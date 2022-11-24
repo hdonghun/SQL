a829aab2477076eca525db9e346ba1a648a2524b
Donghun HAN/韩东勳 <67058000+hdonghun@users.noreply.github.com>
2022-11-24 18:22:24 +0800

    Update 매출분석1.sql

--- SQL데이터분석및시각화/매출분석/매출분석1.sql
+++ SQL데이터분석및시각화/매출분석/매출분석1.sql
@@ -1,15 +1,20 @@
 /************************************
    일/주/월/분기별 매출액 및 주문 건수
 *************************************/
-select date_trunc('day', order_date)::date as day
-	, sum(amount) as sum_amount, count(distinct a.order_id) as daily_ord_cnt
+--orders
+--orderItems
+select date_trunc('month', order_date)::date as month , 
+			sum(amount), count(distinct a.order_id) as monthly_ord_cnt
 from nw.orders a
 	join nw.order_items b on a.order_id = b.order_id
-group by date_trunc('day', order_date)::date 
-order by 1;
+group by date_trunc('month', order_date)::date ;
 
 
-
+select date_trunc('day', order_date)::date as day , 
+			sum(amount), count(distinct a.order_id) as daily_ord_cnt
+from nw.orders a
+	join nw.order_items b on a.order_id = b.order_id
+group by date_trunc('day', order_date)::date ;
 
 
 
