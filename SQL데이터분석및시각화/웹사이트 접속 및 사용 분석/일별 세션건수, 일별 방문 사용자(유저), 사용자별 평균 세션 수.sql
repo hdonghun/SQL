6eae071af44ea3d77f0d49e788fba4c256358d17
Donghun HAN/韩东勳 <67058000+hdonghun@users.noreply.github.com>
2022-12-01 01:24:46 +0800

    Update 일별 세션건수, 일별 방문 사용자(유저), 사용자별 평균 세션 수.sql

--- SQL데이터분석및시각화/웹사이트 접속 및 사용 분석/일별 세션건수, 일별 방문 사용자(유저), 사용자별 평균 세션 수.sql
+++ SQL데이터분석및시각화/웹사이트 접속 및 사용 분석/일별 세션건수, 일별 방문 사용자(유저), 사용자별 평균 세션 수.sql
@@ -19,7 +19,8 @@ with temp_01 as
 	from ga.ga_sess group by to_char(date_trunc('day', visit_stime), 'yyyy-mm-dd')
 )
 select * 
-	, 1.0*daily_sess_cnt/daily_user_cnt as avg_user_sessions
+	, 1.0*daily_sess_cnt/daily_user_cnt as avg_user_sessions --사용자별 평균 세션 수
+	
 	-- 아래와 같이 정수와 정수를 나눌 때 postgresql은 정수로 형변환 함. 1.0을 곱해주거나 명시적으로 float type선언 
 	--, daily_sess_cnt/daily_user_cnt
 from temp_01;
